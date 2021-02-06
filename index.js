3cd7af3c743708498aa58154c51d7031d09bd09b
shubhendra <shubhendrabahadur6@gmail.com>
2021-02-07 02:20:19 +0530

    stylic added using extract style layouts

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express=require('express');
-const app=express();
-const port=8000;
-const expresslayout=require('express-ejs-layouts');
+const express = require("express");
+const app = express();
+const port = 8000;
+const expresslayout = require("express-ejs-layouts");
 
-app.use(express.static('./assets'))
+app.use(express.static("./assets"));
 app.use(expresslayout);
-
+//extract style ans scripts from sub pages
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 // use express router
-app.use('/',require('./routes'))
-app.set('view engine','ejs');
-app.set('views','./views');
+app.use("/", require("./routes"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.listen(port,function(err)
-{
-    if(err)
-    {
-        console.log("Error: ",err);   //console.log(`error: ${err}`);
-        return;
-    }
-    console.log(`server is running successfully on port: ${port}`);
+app.listen(port, function (err) {
+  if (err) {
+    console.log("Error: ", err); //console.log(`error: ${err}`);
+    return;
+  }
+  console.log(`server is running successfully on port: ${port}`);
 });
