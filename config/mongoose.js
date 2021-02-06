6dd3c0f8fb015e69f73a429ba166e56b216fb880
shubhendra <shubhendrabahadur6@gmail.com>
2021-02-07 02:29:45 +0530

    mongodb connected

---
+++ config/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose =require('mongoose');
+mongoose.connect('mongodb://localhost/codeial_develope');
+const db=mongoose.connection;
+
+db.on('error',console.error.bind(console,"there is error in database"));
+db.once('open',function()
+{
+    console.log("database is succesfully running");
+})
+
+module.exports-db;
